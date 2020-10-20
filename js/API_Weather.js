c09f7fecd9f6b5580eaf931c53553a669e0a857b
sahraoui <sahraoui.akram@gmail.com>
2020-10-20 16:01:05 +0200

    last

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -2,31 +2,28 @@
 
 // Clé api
 const API_KEY = "4081444b7b90198136fefe6ed4ccf35b";
+
 // Url API
-const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_URL = "https://api.openweathermap.org/data/2.5/forecast/daily";
+
 // Base source icon
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
 
+class API_WEATHER {
 
-class API_WEATHER{
-  constructor(city){
-    // Si la ville n'est pas définit alors la ville par défault est Paris
-    if(city === undefined){
-      city = "paris";
+    // Faire la requete à l'API openweathermap
+    // Retourne une promise
+    
+
+    // Retourne l'element HTML de l'icon symbolisant la méteo.
+    getHTMLElementFromIcon(icon){
+        return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
     }
-    this.city = city;
-  }
-
-  // Faire la requete à l'API openweathermap
-  // Retourne une promise
-  fetchTodayForecast(){
-    return axios
-    .get(`${API_URL}?q=${this.city}&units=metric&appid=${API_KEY}`, {
-      crossdomain: true
-    })
-  }
-  // Retourne l'element HTML de l'icon symbolisant la méteo.
-  getHTMLElementFromIcon(icon){
-    return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
-  }
+    //pour le jour meme et les 3jours avenir cnt=4
+    fetchThreeDaysForecast(city) {
+        return axios
+        .get(`${API_URL}?q=${city}&cnt=4&units=metric&appid=${API_KEY}`, {
+          crossdomain: true
+        })
+    }
 }
--- js/script.js
+++ js/script.js
@@ -1,31 +1,47 @@
-
-// Fonction appelée lors du click du bouton
 function start() {
-  // Création de l'objet apiWeather
-  const apiWeather = new API_WEATHER();
-  // Appel de la fonction fetchTodayForecast
+var city = document.getElementById("city-input").value;
+if(city === ""){
+  city = "paris";
+}
+// Création de l'objet apiWeather
+const apiWeather = new API_WEATHER(city);
 
-  apiWeather
-    .fetchTodayForecast()
+apiWeather
+
+    .fetchThreeDaysForecast(city)
     .then(function(response) {
-      // Récupère la donnée d'une API
-      const data = response.data;
 
-      // On récupère l'information principal
-      const main = data.weather[0].main;
-      const description = data.weather[0].description;
-      const temp = data.main.temp;
-      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+        console.log(response.data);
+        // Récupère la donnée d'une API
+        const data = response.data;
 
-      // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = main;
-      document.getElementById('today-forecast-more-info').innerHTML = description;
-      document.getElementById('icon-weather-container').innerHTML = icon;
-      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
-      
+        for(i = 0; i <= 3; i++) {
+            htmlElement = "today";
+            if(i == 1) {
+                htmlElement = "jour1";
+            }
+            else if(i == 2) {
+                htmlElement = "jour2";
+            }
+            else if(i == 3) {
+                htmlElement = "jour3";
+            }
+            // On récupère l'information principal
+            const main = data.list[i].weather[0].main;
+            const description = data.list[i].weather[0].description;
+            const temp = data.list[i].temp.day;
+            const icon = apiWeather.getHTMLElementFromIcon(data.list[i].weather[0].icon);
+
+            // Modifier le DOM
+            document.getElementById(htmlElement+'-forecast-main').innerHTML = main;
+            document.getElementById(htmlElement+'-forecast-more-info').innerHTML = description;
+            document.getElementById(htmlElement+'-icon-weather-container').innerHTML = icon;
+            document.getElementById(htmlElement+'-forecast-temp').innerHTML = `${temp}°C`;
+        }
     })
     .catch(function(error) {
-      // Affiche une erreur
-      console.error(error);
+
+        // Affiche une erreur
+        console.error(error);
     });
 }
